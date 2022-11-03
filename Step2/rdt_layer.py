d94e608d0adc77dbcb1b218b5eae296d952e1f98
Gaven Robertson <gaven.robertson@gmail.com>
2022-11-03 16:39:19 -0700

    encapsulated step2 in while loop, testing now.

--- Step2/rdt_layer.py
+++ Step2/rdt_layer.py
@@ -147,45 +147,44 @@ class RDTLayer(object):
         # ########################################################################
         # YOUR CODE HERE:
 
-        while seqnum < 15:
-            data = self.dataToSend[seqnum:seqnum + self.DATA_LENGTH]
-
+        while (seqnum < self.sendWinStart + RDTLayer.FLOW_CONTROL_WIN_SIZE):
 
         # if the seqnum already in the sent list, then 
         # don't send the packet again, since we are waiting for its ack or timeout
         # ########################################################################
         # Step 2: modify the following if statement so it wouldn't return immediately
         # ########################################################################
-        if seqnum in self.dictSent:
-            # YOUR CODE HERE:
-            
-            return
+            if seqnum in self.dictSent:
+                #adding the data length from seqnum and datalength which is just 4
+                seqnum = seqnum + RDTLayer.DATA_LENGTH
+                print("Here is the seq num being moved by .DATALENGTH", seqnum)
+                return
 
         # ##############################################################################################
         # Step 2: add another condition, what if the current segment to be sent exceeds the window size?
         # ##############################################################################################
         # YOUR CODE HERE:
-        if seqnum == 12:
-            return
+            if seqnum > RDTLayer.FLOW_CONTROL_WIN_SIZE:
+                print("exceeds the window size")
+                break
 
-        
         # Get data from the dataToSend
         # ###################################################################################
         # Step 1: modify the following line so that it can send multiple chars in one packet
         # ###################################################################################
         # YOUR CODE HERE:
         
-        data = self.dataToSend[seqnum:seqnum + self.DATA_LENGTH]
+            data = self.dataToSend[seqnum:seqnum + self.DATA_LENGTH]
 
         # create a data segment  
-        seg = Segment()
+            seg = Segment()
 
         # set seqnum and data 
-        seg.setData(seqnum,data)
+            seg.setData(seqnum,data)
 
         # display segment, for debugging purposes
-        print("sending segment: ")
-        seg.dump()
+            print("sending segment: ")
+            seg.dump()
 
         # ###################################################################################
         # Step 4: set seg's start iteration 
@@ -194,11 +193,11 @@ class RDTLayer(object):
 
 
         # Use the unreliable sendChannel to send the segment
-        self.sendChannel.udt_send(seg)
+            self.sendChannel.udt_send(seg)
 
         # Once the segment is sent, add the segment into sent dictionary
         # since now we are waiting for its ack or timeouts
-        self.dictSent[seqnum] = seg
+            self.dictSent[seqnum] = seg
 
 
 
